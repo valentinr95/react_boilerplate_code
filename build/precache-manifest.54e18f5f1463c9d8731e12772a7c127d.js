0218c3a928cbc3419522a4f308c88c6aef3b7117
Valentin Roecklinger <roecklinger.valentin@gmail.com>
2020-07-17 16:55:55 +0200

    added files

---
+++ build/precache-manifest.54e18f5f1463c9d8731e12772a7c127d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7565608b69325243d3da224cc6a87f3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6bc460499ff123e071b0",
+    "url": "/static/js/2.e5493ca5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.e5493ca5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7795197d94c15da94fdb",
+    "url": "/static/js/main.894f44e6.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
